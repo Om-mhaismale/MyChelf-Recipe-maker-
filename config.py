2ac174c80a554307f3a02fa215aa7f3c765c8ec2
om mhaismale <ommhaismale13@gmail.com>
2025-03-31 14:36:17 +0530

    redo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://neondb_owner:npg_mdL80gpntTaF@ep-square-term-a1703bo7.ap-southeast-1.aws.neon.tech/neondb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:1234@localhost/recipe_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)  # or another method to set a secret key
+    SECRET_KEY = os.urandom(24)
